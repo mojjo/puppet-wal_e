ed9dfcb5ae0c251b1abfa0d15efd1330c22070e1
Justin Downing <justin@downing.us>
2014-01-07 16:58:48 -0500

    update dependencies

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -4,23 +4,27 @@ class wal_e::deps {
     package { 'daemontools': ensure => installed }
   }
 
-  if ! defined(Package['lzop']) {
-    package { 'lzop': ensure => installed }
+  if ! defined(Package['git']) {
+    package { 'git': ensure => installed }
   }
 
   if ! defined(Package['libevent-dev']) {
     package { 'libevent-dev': ensure => installed }
   }
 
+  if ! defined(Package['lzop']) {
+    package { 'lzop': ensure => installed }
+  }
+
   if ! defined(Package['pv']) {
     package { 'pv': ensure => installed }
   }
 
-  if ! defined(Package['python-pip']) {
-    package { 'python-pip': ensure => installed }
+  if ! defined(Package['python-dev']) {
+    package { 'python-dev': ensure => installed }
   }
 
-  if ! defined(Package['python-gevent']) {
-    package { 'python-gevent': ensure => installed }
+  if ! defined(Package['python-pip']) {
+    package { 'python-pip': ensure => installed }
   }
 }
