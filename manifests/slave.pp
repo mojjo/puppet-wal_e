8ec0446dd8a501b0053e605c198e4702c60352f9
Justin Downing <justin@downing.us>
2014-01-07 16:59:36 -0500

    use pupplabs-concat for building recovery.conf in wal_e::slave

--- manifests/slave.pp
+++ manifests/slave.pp
@@ -1,12 +1,27 @@
 # == Class: wal_e::slave
 class wal_e::slave {
-  $restore_command  = "restore_command = 'envdir ${wal_e::rootdir}/env /usr/local/bin/wal-e wal-fetch %f %p'"
+  $recovery = "${wal_e::pgdata}/recovery.conf"
 
-  file { "${wal_e::pgdata}/recovery.conf":
-    ensure  => file,
+  concat { $recovery:
     owner   => 'postgres',
     group   => 'postgres',
-    mode    => '0600',
-    content => "${restore_command}\n",
+    mode    => '0640',
+  }
+
+  # if we have a db master host, turn on hot standby
+  if $wal_e::master {
+    $standby_mode = "standby_mode = 'on'\nprimary_conninfo = 'host=${wal_e::master} port=5432'\ntrigger_file = '/var/lib/postgresql/9.2/tmp/pg_failover_trigger'\n"
+    concat::fragment { 'standby_mode':
+      target  => $recovery,
+      content => $standby_mode,
+      order   => '05',
+    }
+  }
+
+  # fetch WAL files from S3
+  concat::fragment { 'restore_command':
+    target  => $recovery,
+    content => "restore_command = 'envdir ${wal_e::rootdir}/env /usr/local/bin/wal-e wal-fetch %f %p'\n",
+    order   => '25',
   }
 }
