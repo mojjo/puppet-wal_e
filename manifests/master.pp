7c8a01e0ad8eb3af972fd4e7e32227230b894213
Justin Downing <justin@downing.us>
2014-01-08 15:29:08 -0500

    add wal_e::master cron job to retain 5 backups

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,9 +1,15 @@
 # == Class: wal_e::master
 class wal_e::master {
-  cron { 'wal_e':
+  cron { 'wal_e-backup-push':
     command => "/usr/bin/envdir ${wal_e::rootdir}/env /usr/local/bin/wal-e backup-push ${wal_e::pgdata}",
     user    => 'postgres',
     minute  => '0',
-    hour    => '4',
+    hour    => '8',
+  }
+  cron { 'wal_e-delete-retain':
+    command => "/usr/bin/envdir ${wal_e::rootdir}/env /usr/local/bin/wal-e delete --confirm retain 5",
+    user    => 'postgres',
+    minute  => '0',
+    hour    => '9',
   }
 }
